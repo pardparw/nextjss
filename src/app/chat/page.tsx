fdfb76a4467784552ba0a3d9435f414661d61a8e
pardparw <87960461+pardparw@users.noreply.github.com>
2023-09-10 12:35:26 +0700

    add css

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import styles from './chat.module.css'
+
 
 export default function chat(){
     return(
-        <Link href='/intro'>Go!</Link>
+        <Link href='/intro' className={styles.go}>Go!</Link>
     )
 }
