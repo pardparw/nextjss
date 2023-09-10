1f2c2d63df1eda37f49bbb7f7f23180f0bb94209
pardparw <87960461+pardparw@users.noreply.github.com>
2023-09-10 11:06:12 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>
-        Hello wolrd
+        Hello wolrd rlddddddddd
       </h1>
        
     </main>
